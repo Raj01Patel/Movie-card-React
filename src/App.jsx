500e25a22a43d4ea902c4c090b8a61c34f133235
Raj Patel <patelraj200208@gmail.com>
2024-05-06 17:31:06 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div>
+      <div className="main-container">
+        <div className='LHS'>
+          <div className="movie-detail">
+            <img src="https://upload.wikimedia.org/wikipedia/en/a/a7/BrightPoster.jpeg"></img>
+            <div className='movie-name'>
+              <h2>Bright</h2>
+              <p style={{ color: "#3a79de" }}>2017 , David ayer</p>
+              <span>117</span>
+              <span> Action,Crime,Fantacy</span>
+            </div>
+          </div>
+          <p>In an LA rife with interspecies tensions, a human cop and his orc partner stumble on a powerful object and become embroiled in a prophesied turf war.In an LA rife with interspecies tensions, a human cop and his orc partner stumble on a powerful object and become embroiled in a prophesied turf war.</p>
+        </div>
+        <div className='RHS'>
+          <img src='https://occ-0-2794-2219.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABWn79DJXgywvB3a0qx8oiPHBNjYZEOF7Fi36pufoCOcq-f38_bBpWuJzKxV8dh1FNmLFQKSjwJBAw5aSTXPUWZj_fW3CRrCMapZH.jpg?r=d99'></img>
+        </div>
+      </div>
+
+      <div className="main-container">
+      <div className='LHS'>
+        <div className="movie-detail">
+          <img src="https://m.media-amazon.com/images/M/MV5BOTY4NDcyZGQtYmVlNy00ODgwLTljYTMtYzQ2OTE3NDhjODMwXkEyXkFqcGdeQXVyNzYzODM3Mzg@._V1_FMjpg_UX1000_.jpg"></img>
+          <div className='movie-name'>
+            <h2>Tomb Raider</h2>
+            <p style={{color:"#3a79de"}}>2018 , Roar Uthung</p>
+            <span>117</span>
+            <span> Action,Crime,Fantacy</span>
+          </div>
+        </div>
+        <p>In an LA rife with interspecies tensions, a human cop and his orc partner stumble on a powerful object and become embroiled in a prophesied turf war.In an LA rife with interspecies tensions, a human cop and his orc partner stumble on a powerful object and become embroiled in a prophesied turf war.</p>
+      </div>
+      <div className='RHS'>
+        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHI0bBrTFMCodQ20n_xB1VBP53oKUxmB47bEIX6MOe_g&s'></img>
+      </div>
+    </div>
+    </div>
+
+
+  )
+}
+
+export default App
